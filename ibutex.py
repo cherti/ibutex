955492565bdca34a99e13a3f27f864b961967482
Felix5721 <f.judge42@gmail.com>
2018-03-29 20:44:54 +0200

    added flag to specify the bibliography backend to use

--- ibutex.py
+++ ibutex.py
@@ -3,15 +3,18 @@
 import os, argparse, glob, shutil, subprocess
 
 latexbase = ['lualatex']
-bibtexbase = ['bibtex']
+bibtexbase = []
 
 parser = argparse.ArgumentParser(description='IbuTex - LaTeX-tooling to reduce pain')
 parser.add_argument('-c', '--clean', action="store_true", dest='cleanbuild', default=False, help='clean build, remove temporary and cached data before')
 parser.add_argument('-q', '--quick', action="store_true", dest='quick', default=False, help='quick build, single run')
 parser.add_argument('-i', '--include', type=str, metavar='file/dir', nargs='+', help='files or folder that need to be linked to the builddir')
 parser.add_argument('--build-only', action="store_false", dest='showpdf', default=True, help='show the compiled documend afterwards')
+parser.add_argument('-b', '--bibcmd', type=str, dest='bibcmd', default="bibtex", help='enter which bibliography backend to use')
 
 args = parser.parse_args()
+
+bibtexbase.append(args.bibcmd)
 
 texfiles = glob.glob('*.tex')
 
