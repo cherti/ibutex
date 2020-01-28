4476ccc02a80f8348f98f3d289a55fb5b14d84fe
Jonas Große Sundrup <cherti-git@letopolis.de>
2020-01-28 21:33:49 +0100

    improve compiler-setting-flags

--- ibutex.py
+++ ibutex.py
@@ -2,19 +2,18 @@
 
 import os, argparse, glob, shutil, subprocess
 
-latexbase = ['lualatex']
-bibtexbase = []
-
 parser = argparse.ArgumentParser(description='IbuTex - LaTeX-tooling to reduce pain')
 parser.add_argument('-c', '--clean', action="store_true", dest='cleanbuild', default=False, help='clean build, remove temporary and cached data before')
 parser.add_argument('-q', '--quick', action="store_true", dest='quick', default=False, help='quick build, single run')
 parser.add_argument('-i', '--include', type=str, metavar='file/dir', nargs='+', help='files or folder that need to be linked to the builddir')
 parser.add_argument('--build-only', action="store_false", dest='showpdf', default=True, help='show the compiled documend afterwards')
+parser.add_argument('-l', '--latexcmd', type=str, dest='latexcmd', default="lualatex", help='enter which latex compiler base command to use')
 parser.add_argument('-b', '--bibcmd', type=str, dest='bibcmd', default="bibtex", help='enter which bibliography backend to use')
 
 args = parser.parse_args()
 
-bibtexbase.append(args.bibcmd)
+latexbase  = args.bibcmd.split()
+bibtexbase = args.latexcmd.split()
 
 texfiles = glob.glob('*.tex')
 
@@ -29,6 +28,7 @@ else:
 
 if len(texfiles) < 1:
 	print(':: no *.tex-files found for compilation')
+	sys.exit(1)
 
 if args.cleanbuild and os.path.exists('.texbuild'):
 	shutil.rmtree('.texbuild')
@@ -47,7 +47,7 @@ fullbib = bibtexbase + [os.path.splitext(texfile)[0]]
 rv = subprocess.call(fullcmd)
 if rv != 0:
 	print(":: error compiling")
-	exit(1)
+	exit(2)
 
 if not args.quick:
 	subprocess.call(fullbib)
